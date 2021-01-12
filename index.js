95bb77bbc940ad7ace8054584bbc77940aaee8d5
Ryan Hardin <ryanwhardin@gmail.com>
2021-01-12 18:45:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(){
+function takeANumber(katzDeli, otherDeli){
   var katzDeli = [];
   var otherDeli = ["Steven", "Blake", "Avi"];
   return 'Welcome, ${katzDeli}. You are number ${otherDeli} in line.'
