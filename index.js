f2bb4b1282abd6f4bfeb9f4ea7786ee0130a4e9f
Ryan Hardin <ryanwhardin@gmail.com>
2021-01-12 19:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function takeANumber(katzDeli, name){
   return 'Welcome, ${name}. You are number ${katzDeli.length} in line.';
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine.shift() + ".";
+function nowServing(katzDeli) {
+  if (katzDeli.length > 0) {
+    return "Currently serving " + katzDeli.shift() + ".";
   } else {
       return "There is nobody waiting to be served!";
   }
 }
 
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
+function currentLine(katzDeli) {
+  if (katzDeli.length > 0) {
     var output = "";
-    for(var i = 0; i < katzDeliLine.length; i++) {
-        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+    for(var i = 0; i < katzDeli.length; i++) {
+        output += (i + 1) + ". " + katzDeli[i] + ", ";
       }
     output = output.slice(0, -2);
     return `The line is currently: ${output}`;
